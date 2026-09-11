0b64b2f1832605e6e75a54fdc9348be3ee6cbb90
Tao <2584300846@qq.com>
2026-09-11 18:27:54 +0800

    Remove .dsh-analysis scratch folder from repo and ignore it

--- .dsh-analysis/cpuid.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-call "C:\Program Files\Microsoft Visual Studio\18\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x64 -vcvars_ver=14.44 >nul
-cl /nologo /O2 /EHsc /Fe"D:\TaoVm\.dsh-analysis\cpuid_probe.exe" /Fo"D:\TaoVm\.dsh-analysis\cpuid_probe.obj" "D:\TaoVm\.dsh-analysis\cpuid_probe.cpp"
-if errorlevel 1 exit /b 1
-"D:\TaoVm\.dsh-analysis\cpuid_probe.exe"
