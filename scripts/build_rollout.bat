d559ab4585ab97a1409e5f3b4ec6a50a7580f9dc
Tao <2584300846@qq.com>
2026-09-12 18:15:31 +0800

    docs: diagnose the CPU decode regression as a bandwidth-wall effect, not a code defect

--- scripts/build_rollout.bat
+++ scripts/build_rollout.bat
@@ -1,5 +1,5 @@
 @echo off
 cd /d D:\TaoVm
 call "C:\Program Files\Microsoft Visual Studio\18\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x64 -vcvars_ver=14.44 >nul 2>&1
-cl /nologo /O2 /EHsc /std:c++17 /arch:AVX2 /DNOMINMAX /DTAO_NO_FFN /DTAO_DELTA_MEM /I src src\grpo_rollout.cpp /Febuild\grpo_rollout.exe /Fobuild\grpo_rollout.obj bcrypt.lib
+cl /nologo /O2 /EHsc /std:c++17 /arch:AVX2 /DNOMINMAX /DTAO_CPU_AVX2 /DTAO_NO_FFN /DTAO_DELTA_MEM /I src src\grpo_rollout.cpp /Febuild\grpo_rollout.exe /Fobuild\grpo_rollout.obj bcrypt.lib
 echo EXITCODE_ROLLOUT=%errorlevel%
